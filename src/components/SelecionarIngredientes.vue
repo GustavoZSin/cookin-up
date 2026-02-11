d31b6a1b7456be733c301e47b6572c8f3a1700e3
Gustavo Zampieri <110244543+GustavoZSin@users.noreply.github.com>
2026-02-10 22:13:32 -0300

    Initial commit with current project files

---
+++ src/components/SelecionarIngredientes.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+    <section class="selecionar-ingredientes">
+        <h1 class="cabecalho titulo-ingredientes">ingredientes</h1>
+
+        <p class="paragrafo-lg instrucoes">Selecione abaixo os ingredientes que você quer usar nesta receita:</p>
+    </section>
+</template>
